0913d20db3eac5f439a1c861c9d2f6a18b3b923b
jbielak <justyna_bielak@outlook.com>
2018-07-25 17:03:31 +0200

    feat: add saving favorite tracks to firebase

--- app/src/main/java/com/jbielak/wordsguide/TrackActivity.java
+++ app/src/main/java/com/jbielak/wordsguide/TrackActivity.java
@@ -3,17 +3,26 @@ package com.jbielak.wordsguide;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.jbielak.wordsguide.adapter.TrackAdapter;
+import com.jbielak.wordsguide.dto.TrackDto;
 import com.jbielak.wordsguide.model.Track;
-import com.jbielak.wordsguide.model.TrackSearchResponse;
 import com.jbielak.wordsguide.model.lyrics.LyricsResponse;
 import com.jbielak.wordsguide.network.MusixmatchApiUtils;
 import com.jbielak.wordsguide.network.MusixmatchService;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -21,6 +30,8 @@ import retrofit2.Response;
 public class TrackActivity extends AppCompatActivity {
 
     private static final String TAG = TrackActivity.class.getSimpleName();
+
+    public static final String FAVORITE_TRACKS_KEY = "favorite_tracks";
 
     @BindView(R.id.tv_title)
     TextView mTitle;
@@ -34,9 +45,17 @@ public class TrackActivity extends AppCompatActivity {
     @BindView(R.id.tv_lyrics)
     TextView mLyrics;
 
+    @BindView(R.id.btn_add_to_favorites)
+    Button mBtnAddToFavorites;
+
     private Track mTrack;
     private MusixmatchService mService;
 
+    private String mUserDisplayName;
+    private FirebaseDatabase mFirebaseDatabase;
+    private DatabaseReference mTracksDatabaseReference;
+    FirebaseAuth mFirebaseAuth;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,6 +63,13 @@ public class TrackActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         mService = MusixmatchApiUtils.getMusixmatchService();
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mFirebaseDatabase = FirebaseDatabase.getInstance();
+        mUserDisplayName = mFirebaseAuth.getCurrentUser().getDisplayName();
+        mTracksDatabaseReference = mFirebaseDatabase.getReference()
+                .child(FAVORITE_TRACKS_KEY)
+                .child(mUserDisplayName);
 
         if (getIntent().hasExtra(TrackAdapter.EXTRA_TRACK)) {
             mTrack = getIntent().getParcelableExtra(TrackAdapter.EXTRA_TRACK);
@@ -57,6 +83,23 @@ public class TrackActivity extends AppCompatActivity {
             if (mTrack.getTrackId() != null) {
                 getLyrics(String.valueOf(mTrack.getTrackId()));
             }
+        }
+
+    }
+
+    @OnClick(R.id.btn_add_to_favorites)
+    protected void addToFavorites() {
+        if (mUserDisplayName != null && !mUserDisplayName.isEmpty()) {
+            TrackDto track = new TrackDto(
+                    mTrack.getTrackId(),
+                    mTrack.getTrackName(),
+                    mTrack.getArtistName(),
+                    mTrack.getAlbumName(),
+                    mTrack.getFirstReleaseDate(),
+                    mTrack.getAlbumCoverart100x100());
+            mTracksDatabaseReference.child(String.valueOf(track.getTrackId())).setValue(track);
+            Toast.makeText(getApplicationContext(), getString(R.string.message_track_added_to_favorites),
+                    Toast.LENGTH_SHORT).show();
         }
     }
 
---
+++ app/src/main/java/com/jbielak/wordsguide/dto/TrackDto.java
@@ -0,0 +1,112 @@
+package com.jbielak.wordsguide.dto;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class TrackDto implements Parcelable {
+
+    private Integer trackId;
+    private String trackName;
+    private String artistName;
+    private String albumName;
+    private String firstReleaseDate;
+    private String albumCoverart100x100;
+
+    public TrackDto() {
+    }
+
+    public TrackDto(Integer trackId, String trackName, String artistName, String albumName,
+                    String firstReleaseDate, String albumCoverart100x100) {
+        this.trackId = trackId;
+        this.trackName = trackName;
+        this.artistName = artistName;
+        this.albumName = albumName;
+        this.firstReleaseDate = firstReleaseDate;
+        this.albumCoverart100x100 = albumCoverart100x100;
+    }
+
+    protected TrackDto(Parcel in) {
+        this.trackId = ((Integer) in.readValue((Integer.class.getClassLoader())));
+        this.trackName = in.readString();
+        this.artistName = in.readString();
+        this.albumName = in.readString();
+        this.firstReleaseDate = in.readString();
+        this.albumCoverart100x100 = in.readString();
+    }
+
+    public static final Creator<TrackDto> CREATOR = new Creator<TrackDto>() {
+        @Override
+        public TrackDto createFromParcel(Parcel in) {
+            return new TrackDto(in);
+        }
+
+        @Override
+        public TrackDto[] newArray(int size) {
+            return new TrackDto[size];
+        }
+    };
+
+    public Integer getTrackId() {
+        return trackId;
+    }
+
+    public void setTrackId(Integer trackId) {
+        this.trackId = trackId;
+    }
+
+    public String getTrackName() {
+        return trackName;
+    }
+
+    public void setTrackName(String trackName) {
+        this.trackName = trackName;
+    }
+
+    public String getArtistName() {
+        return artistName;
+    }
+
+    public void setArtistName(String artistName) {
+        this.artistName = artistName;
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void setAlbumName(String albumName) {
+        this.albumName = albumName;
+    }
+
+    public String getFirstReleaseDate() {
+        return firstReleaseDate;
+    }
+
+    public void setFirstReleaseDate(String firstReleaseDate) {
+        this.firstReleaseDate = firstReleaseDate;
+    }
+
+    public String getAlbumCoverart100x100() {
+        return albumCoverart100x100;
+    }
+
+    public void setAlbumCoverart100x100(String albumCoverart100x100) {
+        this.albumCoverart100x100 = albumCoverart100x100;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeValue(trackId);
+        parcel.writeString(trackName);
+        parcel.writeString(artistName);
+        parcel.writeString(albumName);
+        parcel.writeString(firstReleaseDate);
+        parcel.writeString(albumCoverart100x100);
+
+    }
+}
