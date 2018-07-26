a854b73590233c18f8a38972994ad1f39b8a5c94
jbielak <justyna_bielak@outlook.com>
2018-07-26 17:30:04 +0200

    feat: get device country code

--- app/src/main/java/com/jbielak/wordsguide/ChartsActivity.java
+++ app/src/main/java/com/jbielak/wordsguide/ChartsActivity.java
@@ -1,11 +1,19 @@
 package com.jbielak.wordsguide;
 
+import android.content.res.Resources;
+import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.Locale;
 
 import butterknife.ButterKnife;
 
 public class ChartsActivity extends AppCompatActivity {
+
+    private String currentCountryCode;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +21,19 @@ public class ChartsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_charts);
         ButterKnife.bind(this);
 
+        currentCountryCode = getCountryCode();
+
+    }
+
+    private static String getCountryCode() {
+        String localeCountry;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            localeCountry = Resources.getSystem().getConfiguration()
+                    .getLocales().get(0).getCountry().toLowerCase();
+        } else {
+            localeCountry = Resources.getSystem().getConfiguration()
+                    .locale.getCountry().toLowerCase();
+        }
+        return localeCountry;
     }
 }
